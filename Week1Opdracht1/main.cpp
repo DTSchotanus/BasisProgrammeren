1dbd2b0a79458b6276389df5efef5feebdf76839
p70714405 <d.t.schotanus@windesheim.nl>
2022-09-02 12:24:01 +0200

    add hello world

---
+++ Week1Opdracht1/main.cpp
@@ -0,0 +1,13 @@
+
+// Dit is een comment, die begint met 2x /. Alles wat daarachter komt wordt niet gezien door je compiler
+
+#include <stdio.h> // Dit is het standard library voor input output. Dit is nodig om in- en uitvoer op je console te krijgen
+
+
+int main(void) { // Dit is onze main functie. Deze wordt altijd als eerste aangeroepen
+
+	printf("Hello World!"); // printf print wat tussen haakjes en aanhalingstekens staat op de commandline. Vergeet niet de ;
+
+	return 0; // returnt 0, dat betekent dat het programma zonder fouten is afgedraaid
+}
+
